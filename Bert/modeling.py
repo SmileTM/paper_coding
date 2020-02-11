ad139936af83d92d4a512dc1f651919571f6532e
STM <183637818@qq.com>
2020-02-11 23:47:10 +0800

    add Bert modeling.py

--- Bert/modeling.py
+++ Bert/modeling.py
@@ -9,14 +9,144 @@
 import tensorflow as tf
 import utils
 
+# Todo: bert 与 albert 分离
+class BertConfig(object):
+    def __init__(self,
+                 vocab_size=20000,
+                 model_name='bert',
+                 n_head=12,
+                 d_hidden=768,
+                 num_hidden_layers=12,
+                 d_intermediate=3072,
+                 max_position_embedding=512,
+                 type_vocab_size=16,
+                 hidden_act='gelu',
+                 initializer_range=0.02,
+                 attention_dropout_rate=0.0,
+                 hidden_dropout_rate=0.0):
+        self.vocab_size = vocab_size
+        self.model_name = model_name
+        self.n_head = n_head
+        self.d_hidden = d_hidden
+        self.num_hidden_layers = num_hidden_layers
+        self.d_intermediate = d_intermediate
+        self.max_position_embedding = max_position_embedding
+        self.type_vocab_size = type_vocab_size
+        self.hidden_act = hidden_act
+        self.initializer_range = initializer_range
+        self.attention_dropout_rate = attention_dropout_rate
+        self.hidden_dropout_rate = hidden_dropout_rate
+
+
+class BertModel(tf.keras.layers.Layer):
+    def __init__(self, bertconfig: BertConfig, **kwargs):
+        super(BertModel, self).__init__(**kwargs)
+        self.config = bertconfig
+
+    def build(self, input_shape):
+        self.encoder = Transformer(num_hidden_layers=self.config.num_hidden_layers,
+                                   d_hidden=self.config.d_hidden,
+                                   n_head=self.config.n_head,
+                                   model_name=self.config.model_name,
+                                   d_intermediate=self.config.d_intermediate,
+                                   hidden_act=self.config.hidden_act,
+                                   attention_dropout_rate=self.config.attention_dropout_rate,
+                                   hidden_dropout_rate=self.config.hidden_dropout_rate,
+                                   initializer_range=self.config.initializer_range,
+                                   name='encoder'
+                                   )
+        self.embedding_processor = EmbeddingProcessor(vocab_szie=self.config.vocab_size,
+                                                      d_hidden=self.config.d_hidden,
+                                                      max_position_embedding=self.config.max_position_embedding,
+                                                      type_vocab_size=self.config.type_vocab_size,
+                                                      hidden_dropout_rate=self.config.hidden_dropout_rate,
+                                                      initializer_range=self.config.initializer_range)
+        self.pooler_transform = tf.keras.layers.Dense(
+            units=self.config.d_hidden,
+            activation="tanh",
+            kernel_initializer=get_initializer(self.config.initializer_range),
+            name="pooler_transform")
+
+        super(BertModel, self).build(input_shape)
+
+    def call(self, inputs, mode="bert"):
+        (input_word_ids, input_mask, input_type_ids) = inputs
+        input_tensor = self.embedding_processor((input_word_ids, input_type_ids))
+        attention_mask = create_attention_mask_from_input_mask(input_mask)
+
+        if mode == 'encoder':
+            return self.encoder((input_tensor, attention_mask), return_all_layers=True)
+
+        sequence_output = self.encoder((input_tensor, attention_mask))
+        first_token_tensor = tf.squeeze(sequence_output[:, 0:1, :], axis=1)  # [batch_size ,d_hidden]
+        pooleed_output = self.pooler_transform(first_token_tensor)
+
+        return (pooleed_output, sequence_output)
+
+
+class EmbeddingProcessor(tf.keras.layers.Layer):
+    def __init__(self,
+                 vocab_szie,
+                 d_hidden=768,
+                 max_position_embedding=512,
+                 type_vocab_size=16,
+                 hidden_dropout_rate=0.0,
+                 initializer_range=0.02,
+                 **kwargs):
+        super(EmbeddingProcessor, self).__init__(**kwargs)
+        self.vocab_size = vocab_szie
+        self.d_hidden = d_hidden
+        self.max_position_embedding = max_position_embedding
+        self.type_vocab_size = type_vocab_size
+        self.hidden_dropout_rate = hidden_dropout_rate
+        self.initializer_range = initializer_range
+
+    def build(self, input_shape):
+        self.embedding_word_ids = tf.keras.layers.Embedding(input_dim=self.vocab_size,
+                                                            output_dim=self.d_hidden,
+                                                            embeddings_initializer=get_initializer(
+                                                                self.initializer_range),
+                                                            name="embedding_word_ids",
+                                                            dtype=tf.float32
+                                                            )
+        self.embedding_type_ids = tf.keras.layers.Embedding(input_dim=self.type_vocab_size,
+                                                            output_dim=self.d_hidden,
+                                                            embeddings_initializer=get_initializer(
+                                                                self.initializer_range),
+                                                            name="embedding_type_ids",
+                                                            dtype=tf.float32)
+        self.embedding_pos = self.add_weight(name='embedding_pos',
+                                             shape=(self.max_position_embedding, self.d_hidden),
+                                             initializer=get_initializer(self.initializer_range),
+                                             dtype=tf.float32)
+
+        self.output_layer_norm = tf.keras.layers.LayerNormalization(
+            name="layer_norm", axis=-1, epsilon=1e-12, dtype=tf.float32)
+
+        self.output_dropout = tf.keras.layers.Dropout(
+            rate=self.hidden_dropout_rate, dtype=tf.float32)
+        super(EmbeddingProcessor, self).build(input_shape)
+
+    def call(self, inputs):
+        input_word_ids, input_type_ids = inputs
+        token_word_embeddings = self.embedding_word_ids(input_word_ids)  # [batch_size, seq_length, hidden_size]
+        token_type_embeddings = self.embedding_type_ids(input_type_ids)  # [batch_size, seq_length, hidden_size]
+        token_pos_embeddings = tf.expand_dims(self.embedding_pos, axis=0)  # [1, seq_length, hidden_size]
+
+        output = token_word_embeddings + token_type_embeddings + token_pos_embeddings
+        output = self.output_layer_norm(output)
+        output = self.output_dropout(output)
+        return output
+
 
 class Atttention(tf.keras.layers.Layer):
     def __init__(self,
                  d_hidden=768,
                  n_head=12,
                  dropout_rate=0.0,
-                 initializer_range=0.02):
-        super(Atttention, self).__init__()
+                 initializer_range=0.02,
+                 **kwargs):
+        super(Atttention, self).__init__(**kwargs)
         self.d_hidden = d_hidden
         self.n_head = n_head
         self.dropout_rate = dropout_rate
@@ -66,7 +196,8 @@ class Atttention(tf.keras.layers.Layer):
         qk = tf.einsum('BNFD,BNfD->BNFf', q, k)
         dk = tf.cast(k.shape[-1], qk.dtype)
         attention_weights = qk / tf.sqrt(dk)
-        if attention_mask != None:
+
+        if attention_mask is not None:
             attention_mask = attention_mask[:, None, :, :]
             attention_weights += tf.cast(attention_mask, attention_weights.dtype) * -1e9
 
@@ -84,30 +215,33 @@ class TransformerBlock(tf.keras.layers.Layer):
                  d_intermediate=3072,
                  hidden_act='gelu',
                  initializer_range=0.02,
-                 dropout_rate=0.0):
-        super(TransformerBlock, self).__init__()
+                 hidden_dropout_rate=0.0,
+                 attention_dropout_rate=0.0,
+                 **kwargs):
+        super(TransformerBlock, self).__init__(**kwargs)
         self.n_head = n_head
         self.d_hidden = d_hidden
         self.d_intermediate = d_intermediate
         self.hidden_act = hidden_act
         self.initializer_range = initializer_range
-        self.dropout_rate = dropout_rate
+        self.hidden_dropout_rate = hidden_dropout_rate
+        self.attention_dropout_rate = attention_dropout_rate
 
     def build(self, input_shape):
         self.attention = Atttention(d_hidden=self.d_hidden,
                                     n_head=self.n_head,
-                                    dropout_rate=self.dropout_rate)
+                                    dropout_rate=self.attention_dropout_rate)
         self.attention_layer_norm = tf.keras.layers.LayerNormalization(name="self_attention_layer_norm",
                                                                        axis=-1,
                                                                        epsilon=1e-12,
                                                                        dtype=tf.float32)
-        self.attention_dropout = tf.keras.layers.Dropout(rate=self.dropout_rate)
+        self.attention_dropout = tf.keras.layers.Dropout(rate=self.hidden_dropout_rate)
 
         self.output_layer_norm = tf.keras.layers.LayerNormalization(name="output_layer_norm",
                                                                     axis=-1,
                                                                     epsilon=1e-12,
                                                                     dtype=tf.float32)
-        self.output_dropout = tf.keras.layers.Dropout(rate=self.dropout_rate)
+        self.output_dropout = tf.keras.layers.Dropout(rate=self.hidden_dropout_rate)
 
         self.output_dense1 = tf.keras.layers.Dense(self.d_intermediate,
                                                    activation=utils.get_activation(self.hidden_act),
@@ -132,13 +266,66 @@ class TransformerBlock(tf.keras.layers.Layer):
 
         return layer_output
 
+
 class Transformer(tf.keras.layers.Layer):
-    def __init__(self):
-        super(Transformer, self).__init__()
+    def __init__(self,
+                 model_name='bert',
+                 n_head=12,
+                 d_hidden=768,
+                 num_hidden_layers=12,
+                 d_intermediate=3072,
+                 hidden_act='gelu',
+                 initializer_range=0.02,
+                 attention_dropout_rate=0.0,
+                 hidden_dropout_rate=0.0,
+                 **kwargs):
+        super(Transformer, self).__init__(**kwargs)
+        self.model_name = model_name
+        self.n_head = n_head
+        self.d_hidden = d_hidden
+        self.num_hidden_layers = num_hidden_layers
+        self.d_intermerdiate = d_intermediate
+        self.hidden_act = hidden_act
+        self.initializer_range = initializer_range
+        self.hidden_dropout_rate = hidden_dropout_rate
+        self.attention_dropout_rate = attention_dropout_rate
+
     def build(self, input_shape):
-        pass
-    def call(self, inputs, **kwargs):
-        pass
+        self.layers = []
+        if self.model_name == 'bert':
+            for i in range(self.num_hidden_layers):
+                self.layers.append(TransformerBlock(n_head=self.n_head,
+                                                    d_hidden=self.d_hidden,
+                                                    d_intermediate=self.d_intermerdiate,
+                                                    hidden_act=self.hidden_act,
+                                                    initializer_range=self.initializer_range,
+                                                    hidden_dropout_rate=self.hidden_dropout_rate,
+                                                    attention_dropout_rate=self.attention_dropout_rate,
+                                                    name=("layer_%d" % i)))
+        elif self.model_name == 'albert':
+            self.layers = [TransformerBlock(n_head=self.n_head,
+                                            d_hidden=self.d_hidden,
+                                            d_intermediate=self.d_intermerdiate,
+                                            hidden_act=self.hidden_act,
+                                            initializer_range=self.initializer_range,
+                                            hidden_dropout_rate=self.hidden_dropout_rate,
+                                            attention_dropout_rate=self.attention_dropout_rate,
+                                            name="albert_layer")] * self.num_hidden_layers
+
+        super(Transformer, self).build(input_shape)
+
+    def call(self, inputs, return_all_layers=False):
+        input_tensro, attention_mask = inputs
+        output_tensor = input_tensro
+        all_layer_outputs = []
+        for layer in self.layers:
+            output_tensor = layer((output_tensor, attention_mask))
+            all_layer_outputs.append(output_tensor)
+        if return_all_layers:
+            return all_layer_outputs
+        return all_layer_outputs[-1]
+
+
 def get_initializer(initializer_range=0.02):
     """Creates a `tf.initializers.truncated_normal` with the given range.
 
@@ -151,12 +338,27 @@ def get_initializer(initializer_range=0.02):
     return tf.keras.initializers.TruncatedNormal(stddev=initializer_range)
 
 
+def create_attention_mask_from_input_mask(mask):
+    '''
 
-
+    :param mask: shepe = [batch_size, seq_length]
+    :return: attentino_mask = shape = [batch_size, seq_length, seq_length]
+    '''
+    from_mask = mask[:, :, None]
+    to_mask = mask[:, None, :]
+    attention_mask = from_mask * to_mask
+    return tf.cast(attention_mask, tf.float32)
 
 
 if __name__ == '__main__':
-    input_data = tf.constant(tf.random.uniform((32, 512, 768)))
-    at = TransformerBlock(d_hidden=768, n_head=12)((input_data, None))
-    print(at.shape)
+    input_word_ids = tf.keras.layers.Input(shape=(512,))
+    input_mask = tf.keras.layers.Input(shape=(512,))
+    input_type_ids = tf.keras.layers.Input(shape=(512,))
 
+    config = BertConfig(max_position_embedding=512)
+    bertModel = BertModel(config)
+    output = bertModel((input_word_ids, input_mask, input_type_ids))
+
+    model = tf.keras.Model(inputs=(input_type_ids, input_mask, input_word_ids), outputs=output)
+
+    model.summary()
